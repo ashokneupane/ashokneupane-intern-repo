9500213f65eb734723298f9cc45899f173d212da
Ashok Neupane <neupaneashok8115@gmail.com>
2025-05-08 13:40:20 +1000

    Implementing Internationalization with i18next #19

--- onboarding-react-project/src/main.jsx
+++ onboarding-react-project/src/main.jsx
@@ -1,3 +1,4 @@
+// index.js
 /* eslint-disable react/react-in-jsx-scope */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -7,51 +8,74 @@ import Profile from "./Profile.jsx";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import APIcall from "./api/api_call.jsx";
 import Form from "./Form.jsx";
+import "./i18n.js";
+import { useTranslation } from "react-i18next";
+import { supportedLngs } from "./i18n.js";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <header className="fixed top-2 left-2 right-2 w-full bg-blue-100 shadow-md z-50">
-        <div className="max-w-screen-xl mx-auto px-4 py-4">
-          <nav className="flex">
-            <ul className="flex space-x-6">
-              <li>
-                <Link className="text-blue-500 hover:text-blue-800" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-blue-500 hover:text-blue-800"
-                  to="/profile"
-                >
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-blue-500 hover:text-blue-800"
-                  to="/api-call"
-                >
-                  API Call
-                </Link>
-              </li>
-              <li>
-                <Link className="text-blue-500 hover:text-blue-800" to="/form">
-                  Form
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+function App() {
+  const { t, i18n } = useTranslation();
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/api-call" element={<APIcall />} />
-        <Route path="/form" element={<Form />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-);
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <header className="fixed top-2 left-2 right-2 w-full bg-blue-100 shadow-md z-50">
+          <div className="max-w-screen-xl mx-auto px-4 py-4">
+            <nav className="flex">
+              <ul className="flex space-x-6">
+                <li>
+                  <Link className="text-blue-500 hover:text-blue-800" to="/">
+                    {t("home")}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-blue-500 hover:text-blue-800"
+                    to="/profile"
+                  >
+                    {t("profile")}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-blue-500 hover:text-blue-800"
+                    to="/api-call"
+                  >
+                    {t("api_call")}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-blue-500 hover:text-blue-800"
+                    to="/form"
+                  >
+                    {t("form")}
+                  </Link>
+                </li>
+
+                <select
+                  value={t.resolvedLanguage}
+                  onChange={(e) => i18n.changeLanguage(e.target.value)}
+                >
+                  {Object.entries(supportedLngs).map(([code, name]) => (
+                    <option value={code} key={code}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/api-call" element={<APIcall />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
